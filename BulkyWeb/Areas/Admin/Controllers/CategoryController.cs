6940310aea0ba73011af4c5b09ab60d51378e4e9
Christo Van Wyk <christovanwyk00@gmail.com>
2024-03-16 13:09:02 +0200

    Added Admin And Customer Areas to project and moved Views and Controllers and Chnaged project routing to include Areas

--- BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -5,8 +5,9 @@ using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Reflection.Metadata.Ecma335;
 //test//
-namespace BulkyWeb.Controllers
+namespace BulkyWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -20,7 +21,7 @@ namespace BulkyWeb.Controllers
             return View(objCategoryList);
         }
 
-        public IActionResult Create() 
+        public IActionResult Create()
         {
             return View();
         }
@@ -46,22 +47,22 @@ namespace BulkyWeb.Controllers
 
         public IActionResult Edit(int? id)
         {
-            if(id==null || id == 0)
+            if (id == null || id == 0)
             {
                 return NotFound();
             }
-            Category? categoryFromDb = _unitOfWork.Category.Get(u=>u.Id == id);
+            Category? categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id);
             if (categoryFromDb == null)
             {
                 return NotFound();
-            } 
+            }
             return View(categoryFromDb);
         }
-        
+
 
         [HttpPost]
         public IActionResult Edit(Category obj)
-        {         
+        {
             if (ModelState.IsValid)
             {
                 _unitOfWork.Category.Update(obj);
@@ -97,7 +98,7 @@ namespace BulkyWeb.Controllers
             _unitOfWork.Category.Remove(obj);
             _unitOfWork.Save();
             TempData["success"] = "New Category deleted successfully";
-            return RedirectToAction("Index");           
+            return RedirectToAction("Index");
         }
     }
 }
